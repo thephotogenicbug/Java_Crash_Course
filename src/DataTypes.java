61d9500269a7dbe1fb8181120b485a156d6872b8
thephotogenicbug <navinkumarachari@gmail.com>
2025-06-01 12:05:28 +0530

    Java Crash Course

---
+++ src/DataTypes.java
@@ -0,0 +1,29 @@
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        char c = '=';
+        System.out.println(c);
+
+        boolean bool = true;
+        System.out.println(bool);
+
+        byte b = 2;
+        System.out.println(b);
+
+        short s = 50;
+        System.out.println(s);
+
+        int i = 1000;
+        System.out.println(i);
+
+        long l = 3_000_000_000L;
+        System.out.println(l);
+
+        float f = 3.4F;
+        System.out.println(f);
+
+        double d = 2.7;
+        System.out.println(d);
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        int num = 10;
+        int num2 = 5;
+        int num_3 = 2;
+    }
+}
